f7b2fbce933a4ca1139c952da737afa3a5c43a58
inayat.khan.15602 <inayat.khan.15602@gmail.com>
2023-03-03 19:19:10 +0000

    testing successfull commit of testgitcommit doctype

---
+++ meetings/meetings/doctype/testgitcommit/test_testgitcommit.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Dr Inayat Khan and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testtestgitcommit(FrappeTestCase):
+	pass
